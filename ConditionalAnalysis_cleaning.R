552029b2d169f072abfdc968db1cad4aa6ebfa40
jzhang-jhsph <55093876+jzhang-jhsph@users.noreply.github.com>
2021-01-17 05:11:31 -0500

    Add files via upload

---
+++ ConditionalAnalysis_cleaning.R
@@ -0,0 +1,153 @@
+##############################
+# 1. Analyze by each tissue
+
+tissue_list <- readLines("./GTex_V7_tissue_list.txt")
+
+
+for (tissue in tissue_list){
+    
+    res <- list()
+    
+    load(paste0("./Results/ConditionalAnalysis/RDat/",tissue,".RDat"))
+    
+    PWAS_hit=dat.sentinal.pwas$ID
+    
+    for (i in 1:length(PWAS_hit)){
+        
+        TWAS_hit <- twas.hit[i]
+        TWAS_p <- twas.p[i]
+        Dist_of_hits <- dist[i]
+        Corr_of_hits <- corr[i]
+        PcT_p <- PcT.p[i]
+        TcP_p <- TcP.p[i]
+        
+        res[[PWAS_hit[i]]] <- list(TWAS_hit=TWAS_hit, TWAS_p=TWAS_p,
+                                   Dist_of_hits=Dist_of_hits, Corr_of_hits=Corr_of_hits,
+                                   PcT_p=PcT_p, TcP_p=TcP_p)
+    }
+    
+    TWAS_p <- numeric()
+    TWAS_hit <- character()
+    Dist_of_hits <- integer()
+    Corr_of_hits <- numeric()
+    PcT_p <- numeric()
+    TcP_p <- numeric()
+    sig <- logical()
+    for (i in 1:length(res)){
+        m <- (res[[i]]$TWAS_p < p.twas)
+        m[is.na(m)] <- FALSE
+        sig[i] <- m
+        
+        TWAS_p[i] <- res[[i]]$TWAS_p
+        TWAS_hit[i] <- res[[i]]$TWAS_hit
+        Dist_of_hits[i] <- res[[i]]$Dist_of_hits
+        Corr_of_hits[i] <- res[[i]]$Corr_of_hits
+        PcT_p[i] <- res[[i]]$PcT_p
+        TcP_p[i] <- res[[i]]$TcP_p
+    }
+    
+    a <- data.frame(PWAS_hit=dat.sentinal.pwas$ID,
+                    PWAS_p=signif(dat.sentinal$TWAS.P, 3),
+                    TWAS_hit=ifelse(sig,paste0(TWAS_hit,"*"),TWAS_hit),
+                    TWAS_p=ifelse(sig,paste0(signif(TWAS_p, 3),"*"),TWAS_p),
+                    Dist_of_hits=Dist_of_hits,
+                    Corr_of_hits=signif(Corr_of_hits,3),
+                    PcT_p=signif(PcT_p,3),
+                    TcP_p=signif(TcP_p,3))
+    
+    write_tsv(a, paste0("./Results/ConditionalAnalysis/Tables/",tissue,".txt"))
+    
+}
+
+##############################
+# 2. All-tissue analysis
+
+tissue_list <- readLines("./GTex_V7_tissue_list.txt")
+
+# get all the regional sentinel PWAS genes
+load(paste0("./Results/ConditionalAnalysis/RDat/",tissue_list[1],".RDat"))
+PWAS_hit=dat.sentinal.pwas$ID
+
+# multiple testing correction
+p.twas <- 0.05/sum(readRDS("./GTex_V7_n_gene.rds"))
+
+# for each regional sentinel PWAS gene, pick out the most significant nearby TWAS gene across all tissues
+res <- list()
+for (i in 1:length(PWAS_hit)){
+    
+    TWAS_hit=character()
+    TWAS_p=numeric()
+    Dist_of_hits=numeric()
+    Corr_of_hits=numeric()
+    PcT_p=numeric()
+    TcP_p=numeric()
+    
+    for (j in 1:length(tissue_list)){
+        tissue <- tissue_list[j]
+        
+        load(paste0("./Results/ConditionalAnalysis/RDat/",tissue,".RDat"))
+        TWAS_hit[j] <- twas.hit[i]
+        TWAS_p[j] <- twas.p[i]
+        Dist_of_hits[j] <- dist[i]
+        Corr_of_hits[j] <- corr[i]
+        PcT_p[j] <- PcT.p[i]
+        TcP_p[j] <- TcP.p[i]
+    }
+    
+    res[[PWAS_hit[i]]] <- list(TWAS_hit=TWAS_hit, TWAS_p=TWAS_p,
+                               Dist_of_hits=Dist_of_hits, Corr_of_hits=Corr_of_hits,
+                               PcT_p=PcT_p, TcP_p=TcP_p)
+    
+}
+
+min_TWAS_p <- numeric()
+min_tissue <- character()
+min_TWAS_hit <- character()
+min_Dist_of_hits <- integer()
+min_Corr_of_hits <- numeric()
+min_PcT_p <- numeric()
+min_TcP_p <- numeric()
+sig_tiss <- character()
+N_tiss <- integer()
+for (i in 1:length(res)){
+    m <- (res[[i]]$TWAS_p < p.twas)
+    m[is.na(m)] <- FALSE
+    if(sum(m)>0){
+        sig_tiss[i] <- paste(tissue_list[m],collapse=",")
+        N_tiss[i] <- length(tissue_list[m])
+        min_TWAS_p[i] <- min(res[[i]]$TWAS_p[m])
+        min_tissue[i] <- tissue_list[m][which.min(res[[i]]$TWAS_p[m])]
+        min_TWAS_hit[i] <- res[[i]]$TWAS_hit[m][which.min(res[[i]]$TWAS_p[m])]
+        min_Dist_of_hits[i] <- res[[i]]$Dist_of_hits[m][which.min(res[[i]]$TWAS_p[m])]
+        min_Corr_of_hits[i] <- res[[i]]$Corr_of_hits[m][which.min(res[[i]]$TWAS_p[m])]
+        min_PcT_p[i] <- res[[i]]$PcT_p[m][which.min(res[[i]]$TWAS_p[m])]
+        min_TcP_p[i] <- res[[i]]$TcP_p[m][which.min(res[[i]]$TWAS_p[m])]
+    }else{
+        sig_tiss[i] <- NA
+        N_tiss[i] <- 0
+        min_TWAS_p[i] <- min(res[[i]]$TWAS_p, na.rm = T)
+        min_tissue[i] <- tissue_list[which.min(res[[i]]$TWAS_p)]
+        min_TWAS_hit[i] <- res[[i]]$TWAS_hit[which.min(res[[i]]$TWAS_p)]
+        min_Dist_of_hits[i] <- res[[i]]$Dist_of_hits[which.min(res[[i]]$TWAS_p)]
+        min_Corr_of_hits[i] <- res[[i]]$Corr_of_hits[which.min(res[[i]]$TWAS_p)]
+        min_PcT_p[i] <- res[[i]]$PcT_p[which.min(res[[i]]$TWAS_p)]
+        min_TcP_p[i] <- res[[i]]$TcP_p[which.min(res[[i]]$TWAS_p)]
+    }
+}
+
+a <- data.frame(min_TWAS_Tissue=ifelse(N_tiss!=0,paste0(min_tissue,"*"),min_tissue),
+                min_TWAS_TWAS_hit=ifelse(N_tiss!=0,paste0(min_TWAS_hit,"*"),min_TWAS_hit),
+                min_TWAS_p=ifelse(N_tiss!=0,paste0(signif(min_TWAS_p,3),"*"),min_TWAS_p),
+                Dist_of_hits=min_Dist_of_hits,
+                Corr_of_hits=signif(min_Corr_of_hits,3),
+                PcT_p=signif(min_PcT_p,3),
+                TcP_p=signif(min_TcP_p,3),
+                N_significant_tissues_in_TWAS=N_tiss,
+                all_significant_tissues_in_TWAS=sig_tiss)
+
+write_tsv(a, paste0("./Results/ConditionalAnalysis/all-tissue.txt"))
+
+
+
+
+
---
+++ PWAS.conditional.R
@@ -0,0 +1,135 @@
+
+suppressMessages(library(readr))
+suppressMessages(library(stringr))
+suppressMessages(library(dplyr))
+suppressMessages(library(plink2R))
+
+option_list = list(
+    make_option("--PWAS", action="store", default=NA, type='character',
+                help="Path to PWAS output table. [required]"),
+    make_option("--tissue_list", action="store", default=NA, type='character',
+                help="Path to tissue list for which the conditional analysis is performed [required]"),
+    make_option("--TWAS", action="store", default=NA, type='character',
+                help="Path to the folder containing TWAS output tables. [required]"),
+    make_option("--out", action="store", default=NA, type='character',
+                help="Path to output files [required]"),
+    make_option("--imputed_P", action="store", default=NA, type='character',
+                help="Path to imputed plasma protein table is stored [required]"),
+    make_option("--imputed_T", action="store", default=NA, type='character',
+                help="Path to imputed gene expressions table is stored [required]")
+    
+)
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+tissue_list <- readLines(opt$tissue_list)
+
+dat.pwas <- read_tsv(opt$PWAS)
+p.pwas <- 0.05/nrow(dat.pwas)
+dat.pwas <- dat.pwas[!(is.na(dat.pwas$PWAS.P)),]
+# z <- dat.pwas$PWAS.P; z <- z[!(is.na(z))]; z <- qnorm(z/2); pwas.lambdagc <- round(median(z^2)/0.454, 3)
+
+dat.sentinel <- tibble()
+dat <- dat.pwas[dat.pwas$PWAS.P < p.pwas,]
+for(i in 1:22){
+    tmp0 <- dat[dat$CHR == i,]
+    if(nrow(tmp0)==0){
+        next()
+    }
+    tmp <- data.frame(file=tmp0$FILE,
+                      pval=tmp0$PWAS.P,
+                      tss=tmp0$P0,
+                      num=1:nrow(tmp0))
+    res <- integer()
+    while (nrow(tmp)>0) {
+        flag <- which(tmp$pval==min(tmp$pval))[1]
+        res <- c(res, tmp$num[flag])
+        tmp <- tmp[which((tmp$tss > tmp$tss[flag] + 1000000) | tmp$tss < tmp$tss[flag] - 1000000),]
+    }
+    tmp0 <- tmp0[res,]
+    dat.sentinel <- rbind(dat.sentinel, tmp0)
+}
+dat.sentinel <- dat.sentinel[!is.na(dat.sentinel$PWAS.P),]
+dat.sentinel.pwas <- dat.sentinel
+
+pred_prot <- read_tsv(opt$imputed_P)
+
+for (tissue in tissue_list){
+    
+    pred_ge <- read_tsv(paste0(opt$imputed_T,"/", tissue,".txt"))
+    
+    dat.twas <- read_tsv(paste0(opt$TWAS,"/",tissue,".out"))
+    
+    dat.twas <- dat.twas[!(is.na(dat.twas$P0)),]
+    dat.twas <- dat.twas[!(is.na(dat.twas$TWAS.P)),]
+    
+    #z <- dat.twas$TWAS.P; z <- z[!(is.na(z))]; z <- qnorm(z/2); twas.lambdagc <- round(median(z^2)/0.454, 3)
+    
+    PcT.z <- numeric()
+    PcT.p <- numeric()
+    TcP.z <- numeric()
+    TcP.p <- numeric()
+    twas.p <- numeric()
+    twas.hit <- character()
+    dist <- integer()
+    corr <- numeric()
+    for (i in 1:nrow(dat.sentinel.pwas)) {
+        chr <- dat.sentinel.pwas$CHR[i]
+        dat.twas.tmp <- dat.twas[dat.twas$CHR == chr, ]
+        
+        tmp <- dat.twas.tmp[(dat.twas.tmp$P0 < dat.sentinel.pwas$P0[i] + 10^6) & (dat.twas.tmp$P0 > dat.sentinel.pwas$P0[i] - 10^6),]
+        if(nrow(tmp)==0){
+            PcT.z[i] <- NA
+            TcP.z[i] <- NA
+            PcT.p[i] <- NA
+            TcP.p[i] <- NA
+            twas.p[i] <- NA
+            twas.hit[i] <- NA
+            dist[i] <- NA
+            corr[i] <- NA
+        }else{
+            dat.twas.tmp <- tmp[which.min(tmp$TWAS.P),]
+            dist[i] <- dat.twas.tmp$P0 - dat.sentinel.pwas$P0[i]
+            
+            twas.p[i] <- dat.twas.tmp$TWAS.P
+            twas.hit[i] <- dat.twas.tmp$ID
+            
+            pred_mat = matrix(nrow=498,ncol=2)
+            tmp <- strsplit(dat.sentinel.pwas$FILE[i], "/")[[1]]; tmp <- tmp[length(tmp)]; tmp <- substr(tmp, start=1, stop=nchar(tmp)-9)
+            pred_mat[,1] = pred_prot[[tmp]]
+            tmp <- strsplit(dat.twas.tmp$FILE[i], "/")[[1]]; tmp <- tmp[length(tmp)]; tmp <- substr(tmp, start=nchar(dat.twas.tmp$PANEL[i])+2, stop=nchar(tmp)-9)
+            pred_mat[,2] = pred_ge[[tmp]]
+            
+            pred_mat = scale( pred_mat )
+            
+            # compute cis-regulated genetic correlation
+            corr_mat = cor(pred_mat)
+            corr[i] = corr_mat[1,2]
+            
+            b.se = sqrt(1 - corr_mat[1,2]^2)
+            
+            # estimate conditional effect size (P conditional on T, P|T)
+            b = dat.sentinel.pwas$PWAS.Z[i] - corr[i] * dat.twas.tmp$TWAS.Z
+            PcT.z[i] = b / b.se
+            PcT.p[i] = 2*(pnorm( abs( PcT.z[i] ) , lower.tail=F))
+            
+            # estimate conditional effect size (T conditional on P, T|P)
+            b = dat.twas.tmp$TWAS.Z - corr[i] * dat.sentinel.pwas$PWAS.Z[i]
+            TcP.z[i] = b / b.se
+            TcP.p[i] = 2*(pnorm( abs( TcP.z[i] ) , lower.tail=F))
+            
+        }
+        
+    }
+    
+    save(dat.sentinel.pwas,
+         PcT.z, PcT.p,
+         TcP.z, TcP.p,
+         twas.p, twas.hit,
+         dist, corr, 
+         # pwas.lambdagc, twas.lambdagc
+         file = paste0(opt$out, "/",tissue,".RDat"))
+    
+}
+
+
